9bfc10fb3b68cf9515e9cbdc79b6bded1298bebe
bdhamithkumara <damithkumararoxx55@gmail.com>
2025-01-18 23:25:26 +0530

    feat : AboutHero section dynamic data update

--- src/components/aboutComponents/AboutHero.jsx
+++ src/components/aboutComponents/AboutHero.jsx
@@ -7,9 +7,28 @@ import Loader from '../Loader';
 import { useSelector } from 'react-redux';
 
 const AboutHero = () => {
+
+    let [image, setImage] = useState();
+    const [heading, setHeading] =
+      useState();
+    const [Paragraph, setParagraph] =
+      useState();
+
   let about = useSelector(
     state => state.about.aboutPage,
   );
+
+  useEffect(() => {
+  if (about) {
+    let data = about.AboutHero;
+    setImage(data.image)
+    setHeading(data.Heading)
+    setParagraph(data.paragraph)
+  }
+  }, [])
+  
+
+
 
   return !about ? (
     <Loader />
@@ -18,30 +37,20 @@ const AboutHero = () => {
       <div className='about_hero relative w-full h-[90vh] bg-gradient-to-b from-white via-gray-700 to-black'>
         <img
           className='w-full h-full object-cover opacity-75 blur-[1.5px] object-top'
-          src={About_BG}
+          src={image}
           alt=''
         />
 
         <div className='txt w-full h-full absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex items-center justify-center gap-5 max-[599px]:gap-6 flex-col pt-10 max-[599px]:pt-20'>
           <h1 className='w-1/2 max-[599px]:w-full text-center leading-tight text-[3.33vw] max-[599px]:text-[10vw] font-bold font-[Montserrat]'>
-            Let’s learn about new
-            Knowledge and abitlites
+            {heading}
           </h1>
           <p className='w-[58%] max-[599px]:w-[85%] text-[1.25vw] max-[599px]:text-[3.5vw] font-medium text-center font-[Poppins]'>
-            Welcome to [Institute Name],
-            an institution dedicated to
-            fostering innovation,
-            knowledge, and personal
-            growth. Our mission is to
-            shape tomorrow’s leaders by
-            offering exceptional
-            educational opportunities
-            and encouraging intellectual
-            exploration.
+            {Paragraph}
           </p>
           <div className='btns flex items-center gap-4 max-[599px]:mt-4'>
             <button className='bg-transparent text-[1vw] max-[599px]:text-[2.5vw] border-[1px] border-white px-3 py-2 rounded-md font-bold'>
-              Let's Started
+              Let&#39;s Started
             </button>
             <button className='bg-white text-[1vw] max-[599px]:text-[2.5vw] text-black px-4 py-2 rounded-md font-bold'>
               Watch Now
