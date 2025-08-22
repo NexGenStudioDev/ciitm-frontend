b6005309758175f2d61b4e26045045936645a5a4
Abhishek Kumar <abhishek9661342993@gmail.com>
2025-08-22 23:03:52 +0530

    feat: Add Success page and integrate payment verification; update payment redirect URL

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
    server: {
       proxy: {
          '/api': {
-            target: 'https://ciitm-backend.onrender.com',
+            target: 'http://localhost:7000',
             changeOrigin: true,
             secure: false, // Set to true if using HTTPS
          },
